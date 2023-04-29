220f9ac606f136e2e130974f4501604cf5edee5d
@Maquiavélik <127151881+AdamJesusM@users.noreply.github.com>
2023-04-28 22:48:37 -0400

    Add files via upload

---
+++ Palabras_reservadas.py
@@ -0,0 +1,75 @@
+#and = Operador logico
+
+#del = Para eliminar un objeto
+
+#for = Utilizado para hacer bucles determinados
+
+#is = Para probar si las dos varibles se refieren al mismo objeto
+
+#raise = Para crear exepciones como "try y except"
+
+#assert = Se utiliza con fines de depuracion
+
+#if = Se usa en declaraciones condicionales como "else y elif"
+
+#else = Se usa para crear declaraciones condicionales como "elif y if" 
+
+#elif = Se usa para crear condicionales como "else y if"
+
+#from = Para importar partes específicas de un módulo
+
+#lambda = Para crear una funcion anonima
+
+#return = Se usa dentro de una funcion para salir y devolver un valor
+
+#break = Se utiliza en el interior de los bucles "for y while" para alterar su comportamiento
+
+#global = Se utiliza para declarar una variable global
+
+#not = Operador lógico
+
+#try = Se usa para crear exepciones igual que "except y raise"
+
+#class = Se usa para definir una nueva clase definida por el usuario
+
+#exept = Se utiliza par crear excepciones igual que "raise y try"
+
+#or = Operador lógico 
+
+#while = Se utiliza para realizar bucles
+
+#continue = Se utiliza en el interior de los bucles "for y while" para alterar su comportamiento
+
+#exec = Permite ejecutar código Python contenido en una cadena o en un archivo
+
+#import = Para importar un modulo
+
+#yield = Se usa dentro de una funcion al igual que "return" pero devuelve un generador
+
+#def = Se usa para definir una funcion definida por el usuario
+
+#finally = Su uso garantiza que el bloque de codigo dentro de el se ejecute incluso si hay una excepcion no controlada
+
+#in = Para comprobar si un valor esta presente en una lista, tulpa, devuelve true y false
+
+#print = Imprimir en pantalla.
+
+
+
+#no pueden ser ultilizadas para nombrar variables u otros elementos
+
+
+
+#nonlocal = Para declarar una variable no local
+
+#pass = Declaracion nula (marcador de posición)
+
+#async
+
+#as =  Para crear un alias al importar un modulo
+
+#false = Resultado de operaciones o comparaciones logicas
+
+#true = Resultado de operaciones o comparaciones logicas
+
+#none = Representa valor nulo
